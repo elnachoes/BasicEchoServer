7793eb03885bf57254b305005ecc9d5bacab6df2
elnachoes <corbin.martin@protonmail.com>
2021-12-11 21:07:43 -0800

    Add project files.

---
+++ ChatClient/ChatClientProgram.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class ChatClientProgram
+{
+    static void Main()
+    {
+        string UserInput = string.Empty;
+        string Data = string.Empty;
+        byte[] ByteBuffer;
+        int BytesTransferred;
+
+        IPHostEntry Host = Dns.GetHostEntry("localhost");
+        IPAddress IpAddress = Host.AddressList[0];
+        IPEndPoint ServerEndPoint = new IPEndPoint(IpAddress, 11000);
+
+        // Create a TCP/IP  socket.
+        Socket ClientSocket = new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            ClientSocket.Connect(ServerEndPoint);
+            Console.WriteLine("connected to the server...");
+            ByteBuffer = new byte[1024];
+            BytesTransferred = ClientSocket.Receive(ByteBuffer);
+            Console.WriteLine($"{Encoding.ASCII.GetString(ByteBuffer)}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return;
+        }
+
+
+
+        Console.WriteLine("Type 'exit' when you are ready to exit");
+        while (true)
+        {
+            ByteBuffer = new byte[1024];
+            UserInput = Console.ReadLine();
+            
+            if (UserInput == "exit")
+            {
+                break;
+            }
+
+            BytesTransferred = ClientSocket.Send(Encoding.ASCII.GetBytes(UserInput));
+            if (BytesTransferred == 0)
+            {
+                break;
+            }
+            BytesTransferred = ClientSocket.Receive(ByteBuffer);
+            if (BytesTransferred == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine($"{Encoding.ASCII.GetString(ByteBuffer)}");
+        }
+
+        ClientSocket.Shutdown(SocketShutdown.Both);
+        ClientSocket.Close();
+    }
+}
---
+++ ChatServer/ChatServerProgram.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class ChatServerProgram
+{
+    static void Main()
+    {
+        //set up socket
+        IPHostEntry Host = Dns.GetHostEntry("localhost");
+        IPAddress IpAddress = Host.AddressList[0];
+        IPEndPoint ServerEndPoint = new IPEndPoint(IpAddress, 11000);
+        Socket ServerSocket = new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        //
+        ServerSocket.Bind(ServerEndPoint);
+        ServerSocket.Listen(10);
+
+        //
+        string Data = string.Empty;
+        byte[] ByteBuffer;
+        int BytesTransferred;
+
+        //
+        Console.WriteLine("Waiting for a connection...");
+        ServerSocket = ServerSocket.Accept();
+        Console.WriteLine("someone connected...");
+
+        //
+        Data = "send messages and the server will echo them back\n";
+        ServerSocket.Send(Encoding.ASCII.GetBytes(Data));
+
+        //
+        Data = string.Empty;
+        while (true)
+        {
+            ByteBuffer = new byte[1024];
+
+            BytesTransferred = ServerSocket.Receive(ByteBuffer);
+            if (BytesTransferred == 0)
+            {
+                break;
+            }
+
+            BytesTransferred = ServerSocket.Send(ByteBuffer);
+            if (BytesTransferred == 0)
+            {
+                break;
+            }
+        }
+
+        //
+        ServerSocket.Shutdown(SocketShutdown.Both);
+        ServerSocket.Close();
+
+        //
+        Console.WriteLine("press any key to close...");
+        Console.ReadKey();
+    }
+}
